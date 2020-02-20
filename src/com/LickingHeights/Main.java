4deeb26bb12f88fc1dd3784087810f7354491c23
Daniel <daniel.burns.2022@student.lhschools.org>
2020-02-20 10:50:42 -0500

    Gas Receipt Commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,34 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("+-------------------------------+");
+        System.out.println("| Platinum Bay Station          |");
+        System.out.println("|                               |");
+        System.out.println("| Address:                      |");
+        System.out.println("| 2456 Clarence Court           |");
+        System.out.println("| Rancho Cucamonga, CA 91730    |");
+        System.out.println("|                               |");
+        System.out.println("| Date:                   Time: |");
+        System.out.println("| 1/17/2019               17:23 |");
+        System.out.println("|                               |");
+        System.out.println("| Invoice Number:        378271 |");
+        System.out.println("|                               |");
+        System.out.println("| Autherization Number:   62745 |");
+        System.out.println("|                               |");
+        System.out.println("| Mastercard                    |");
+        System.out.println("| **** **** **** **47           |");
+        System.out.println("|                               |");
+        System.out.println("| Pump:     Product:   $/Gallon |");
+        System.out.println("| 07        UNLD       $2.63    |");
+        System.out.println("|                               |");
+        System.out.println("| Amount:                Total: |");
+        System.out.println("| 19.00                  $49.97 |");
+        System.out.println("|                               |");
+        System.out.println("|         Thank you for         |");
+        System.out.println("|    stopping by Platinum Bay   |");
+        System.out.println("|                               |");
+        System.out.println("+-------------------------------+");
+    }
+}
